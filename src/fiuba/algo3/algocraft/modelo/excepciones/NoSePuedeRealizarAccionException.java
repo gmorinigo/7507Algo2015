ac39cae22ae2b07f66704a34c052739fcd3118fb
Josue Vargas <jos.vargasv@gmail.com>
2015-06-13 02:02:31 -0300

    Agregada exception NoSePuedeRealizarAccionExceptino

---
+++ src/fiuba/algo3/algocraft/modelo/excepciones/NoSePuedeRealizarAccionException.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.algocraft.modelo.excepciones;
+
+public class NoSePuedeRealizarAccionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
