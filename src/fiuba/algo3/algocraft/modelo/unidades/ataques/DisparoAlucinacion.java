b3dca284ad811bd35141c5f58d82b4f75e79c850
Josue Vargas <jos.vargasv@gmail.com>
2015-06-17 00:49:36 -0300

    Mas tipos de disparos

---
+++ src/fiuba/algo3/algocraft/modelo/unidades/ataques/DisparoAlucinacion.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.algocraft.modelo.unidades.ataques;
+
+import fiuba.algo3.algocraft.modelo.mapa.Celda;
+import fiuba.algo3.algocraft.modelo.unidades.Unidad;
+
+public class DisparoAlucinacion extends AbstractDisparo {
+
+	public DisparoAlucinacion(Unidad unidad, int radio) {
+		super(unidad, radio);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean disparar(Celda objetivo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/fiuba/algo3/algocraft/modelo/unidades/ataques/DisparoEMP.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.algocraft.modelo.unidades.ataques;
+
+import fiuba.algo3.algocraft.modelo.mapa.Celda;
+import fiuba.algo3.algocraft.modelo.unidades.Unidad;
+
+public class DisparoEMP extends AbstractDisparo{
+
+	public DisparoEMP(Unidad unidad, int radio) {
+		super(unidad, radio);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean disparar(Celda objetivo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
