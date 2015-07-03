abda709bcc6d7b4855d962afce03b6207e9e7bc1
gmorinigo <gmorinigo@users.noreply.github.com>
2015-07-02 22:49:40 -0300

    Se le aplica color a las unidades y construcciones. Inicial azul

--- src/fiuba/algo3/algocraft/vista/VistaObjetoDibujable.java
+++ src/fiuba/algo3/algocraft/vista/VistaObjetoDibujable.java
@@ -1,13 +1,19 @@
 package fiuba.algo3.algocraft.vista;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Observer;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+
 import fiuba.algo3.algocraft.AlgoCraft;
 import fiuba.algo3.algocraft.modelo.mapa.Celda;
 import fiuba.algo3.algocraft.modelo.mapa.Posicion;
@@ -56,22 +62,47 @@ public abstract class VistaObjetoDibujable implements Observer{
 			e.printStackTrace();
 		}
 		
-		//Image otraimg = img.getScaledInstance(115,230,java.awt.Image.SCALE_SMOOTH); //creamos una imagen nueva dándole las dimensiones que queramos a la antigua
-
-		
 		if (unaCelda.tieneConstruccion() && !unaCelda.obtenerConstruccion().construccionRecolectoraDeGas() && !unaCelda.obtenerConstruccion().construccionRecolectoraDeMineral()){
 			if (unaCelda.getPosicion().compararPosicion(unaCelda.obtenerConstruccion().damePosicionCeldaSupIzquierda())){
 				Image imagenEscalada = image.getScaledInstance(60, 60, Image.SCALE_SMOOTH);
-				imageIcon = new ImageIcon(imagenEscalada);
+				BufferedImage imagenConFiltro = this.filtrarAzul(imagenEscalada);
+				imageIcon = new ImageIcon(imagenConFiltro);
 				imageIcon.paintIcon(vistaEscenario, g, posPixel.dameColumna(), posPixel.dameFila());
 			}
 		}
 		else{
-			Image imagenEscalada = image.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-			imageIcon = new ImageIcon(imagenEscalada);
-			imageIcon.paintIcon(vistaEscenario, g, posPixel.dameColumna(), posPixel.dameFila());
+			if (unaCelda.tieneConstruccion() || unaCelda.tieneUnidad()){
+				Image imagenEscalada = image.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+				BufferedImage imagenConFiltro = this.filtrarAzul(imagenEscalada);
+				imageIcon = new ImageIcon(imagenConFiltro);
+				imageIcon.paintIcon(vistaEscenario, g, posPixel.dameColumna(), posPixel.dameFila());
+			}
+			else{
+				Image imagenEscalada = image.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+				imageIcon = new ImageIcon(imagenEscalada);
+				imageIcon.paintIcon(vistaEscenario, g, posPixel.dameColumna(), posPixel.dameFila());
+			}
 		}
 	}
 
+	public BufferedImage filtrarAzul(Image imagenEscalada){
+		
+		//Creación BufferedImage con la imagen
+		BufferedImage bi = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleImage(imagenEscalada.getWidth(null), imagenEscalada.getHeight(null), Transparency.OPAQUE);
+		bi.getGraphics().drawImage(imagenEscalada, 0, 0, null);
+		
+		//Crea una copia del mismo tamaño que la imagen
+		BufferedImage biDestino = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleImage(bi.getWidth(), bi.getHeight(), Transparency.OPAQUE);
+		//Recorre las imagenes y obtiene el color de la imagen original y la almacena en el destino
+		for (int x=0;x < bi.getWidth();x++){
+			for (int y=0; y < bi.getHeight();y++){
+				//Obtiene el color
+				Color c1=new Color(bi.getRGB(x, y));
+				// Color RGB -> Fuerzo el Azul
+				biDestino.setRGB(x, y, new Color(c1.getRed(),c1.getGreen(),150).getRGB());
+			}
+		}
+		return biDestino;
+	}
 }
 
